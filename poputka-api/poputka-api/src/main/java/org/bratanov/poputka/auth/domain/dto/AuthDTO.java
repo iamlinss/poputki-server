b5a3b83e003c932f5f8fb81d4cb4330f67f8265d
iamlinss <smirnovaalina53@gmail.com>
2024-10-31 23:26:25 +0300

    delet old directory

--- poputka-api/poputka-api/src/main/java/org/bratanov/poputka/auth/domain/dto/AuthDTO.java
+++
@@ -1,9 +0,0 @@
-package org.bratanov.poputka.auth.domain.dto;
-
-public class AuthDTO {
-    public record LoginRequest(String email, String password) {
-    }
-
-    public record Response(String token) {
-    }
-}
--- poputka-api/poputka-api/src/main/java/org/bratanov/poputka/repositories/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.bratanov.poputka.repositories;
-
-import org.bratanov.poputka.domain.entities.CityEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends JpaRepository<CityEntity, Long> {
-}
